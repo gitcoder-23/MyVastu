390deb87c6d9fc3ccdbf8189bbfc924b481124ca
gitcoder-23 <prabhatece21@gmail.com>
2026-02-26 22:45:51 +0530

    dyna started

---
+++ src/app/api/config.ts
@@ -0,0 +1,10 @@
+export const baseUrl = 'https://api.bastu.dtftsolutions.com/api/v1';
+
+// Endpoint list
+export const registerApi = '/auth/register';
+export const loginApi = '/auth/login';
+export const logoutApi = '/auth/logout';
+export const refreshTokenApi = '/auth/refresh';
+export const forgotPasswordApi = '/auth/forgot-password';
+export const getProfileApi = '/auth/profile';
+
