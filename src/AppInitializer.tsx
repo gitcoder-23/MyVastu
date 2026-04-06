3192b28bcc2f2540c02f7a1f8c2c306db7fdc9db
gitcoder-23 <prabhatece21@gmail.com>
2026-04-06 18:40:56 +0530

    added

--- src/AppInitializer.tsx
+++ src/AppInitializer.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import AppNavigation from '../app_navigation/AppNavigation';
-import { COLORS } from '../constants/colors';
-import AppStatusBar from '../app_header/AppStatusBar';
-import { useAppSelector } from '../app/redux/hooks';
-import { resetInterceptor } from '../app/api/rootApi';
 import React from 'react';
+import { useAppSelector } from './app/redux/hooks';
+import { resetInterceptor } from './app/api/rootApi';
+import AppStatusBar from './app_header/AppStatusBar';
+import AppNavigation from './app_navigation/AppNavigation';
+import { COLORS } from './constants/colors';
 
 export const AppInitializer = () => {
   const { accessToken } = useAppSelector(state => state.authApp);
